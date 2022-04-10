6bca676739a47ec22fcf72dbff689f3bf399a6f0
Khalfan1998 <khalfan.ikns@gmail.com>
2022-04-10 13:14:13 +0300

    AlSukkariya New start

---
+++ server/data/Products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name: "Sukari Large",
+    image: "/images/sukariLarge.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 7.5,
+    countInStock: 999,
+    rating: 5,
+    numReviews: 4,
+  },
+  {
+    name: "Sukari Galaxy",
+    image: "/images/sukariGalaxy.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 6.5,
+    countInStock: 999,
+    rating: 5,
+    numReviews: 2,
+  },
+];
+
+export default products;
